808ed86da7ebf491010d0f836f60b099cc2a94f2
cheeseisdisgusting <cheeseisdisgusting@gmail.com>
2016-12-12 19:03:22 -0500

    Changed input field and button size

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,10 @@ var getMetarButton = document.getElementById("getMetarButton"), useLocationButto
 // Set up a listener for given station identifier button click
 getMetarButton.addEventListener('click', function(event) {
 	var station = document.getElementById("stationIdentifier").value;
+	document.getElementById("main-instruction").style.display = "none";
+	document.getElementById("stationIdentifier").style.display = "none";
+	document.getElementById("getMetarButton").style.display = "none";
+	document.getElementById("useLocationButton").style.display = "none";
 	fetchMetar(station);
 });
 
@@ -20,34 +24,36 @@ function request(URL) {
 	xmlRequest.open("GET", URL, true); // Initialize and send the request
 	xmlRequest.send();
 
-	var resultPromise = new Promise(
+	var resultPromise = new Promise( // Because fetching METAR is async, promise to return value when fetched
 		function(resolve, reject) {
-			xmlRequest.onreadystatechange = function() { // Fires on ready state change; if the request is done, then the METAR is displayed
+			xmlRequest.onreadystatechange = function() { // Fires on ready state change; if the request is done, then return response
 				if (this.readyState == XMLHttpRequest.DONE) {
 					resolve(xmlRequest.response);
 				}
 			};
 		})
 
-	return resultPromise;
+	return resultPromise; // Return this promise
 }
 
 // Function to get METAR provided station identifer
 function fetchMetar(station) {
-	var URL = "http://avwx.rest/api/metar/" + station;
 
-	var raw = document.createElement('div');
-	request(URL).then(function(result) {
-		metar = JSON.parse(result);
+	var URL = "http://avwx.rest/api/metar/" + station; // This is the URL
 
-		if (metar["Raw-Report"] !== undefined) {
+	var raw = document.createElement('div'); // This creates a new div to display the raw METAR
+	request(URL).then(function(result) { // Wait for promise to be fulfilled, and then do things with the response
+		metar = JSON.parse(result); // Parse JSON
+
+		if (metar["Raw-Report"] !== undefined) { // If there is a raw-report field in the JSON, then show that in the text
 			raw.innerHTML = metar["Raw-Report"];
-		} else {
+		} else { // If there isn't, tell the user that their query was invalid
 			raw.innerHTML = "Your request was invalid!"
 		}
 
-		document.getElementById("location").appendChild(raw);
-	}).catch(function() {
+		document.getElementById("location").appendChild(raw); // Add to the webpage!
+	}).catch(function(reason) { // This means that the query was rejected for some reason
+		console.log(reason); // Log the reason and tell the user
 		raw.innerHTML = "Your request was invalid!"
 		document.getElementById("location").appendChild(raw);
     });
