d4c952f92483205794a2e37ef1b91884c74ae889
cheeseisdisgusting <cheeseisdisgusting@gmail.com>
2016-12-12 19:58:54 -0500

    Added a return button
    There is now a button to go back to the main functionality.

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 console.log("JS initialized");
 
-var getMetarButton = document.getElementById("getMetarButton"), useLocationButton = document.getElementById("useLocationButton");
+var getMetarButton = document.getElementById("getMetarButton"), useLocationButton = document.getElementById("useLocationButton"), returnButton = document.getElementById("returnButton");
 
 // Set up a listener for given station identifier button click
 getMetarButton.addEventListener('click', function(event) {
@@ -9,6 +9,7 @@ getMetarButton.addEventListener('click', function(event) {
 	document.getElementById("stationIdentifier").style.display = "none";
 	document.getElementById("getMetarButton").style.display = "none";
 	document.getElementById("useLocationButton").style.display = "none";
+	document.getElementById("returnButton").style.display = "inline-block";
 	fetchMetar(station);
 });
 
@@ -16,6 +17,16 @@ getMetarButton.addEventListener('click', function(event) {
 useLocationButton.addEventListener('click', function(event) {
 	console.log("The use location button has been pressed!");
 });
+
+// Set up even listener for using the return button click
+returnButton.addEventListener('click', function(event) {
+	document.getElementById("main-instruction").style.display = "block";
+	document.getElementById("stationIdentifier").style.display = "inline-block";
+	document.getElementById("getMetarButton").style.display = "inline-block";
+	document.getElementById("useLocationButton").style.display = "inline-block";
+	document.getElementById("returnButton").style.display = "none";
+	document.getElementById("raw").parentNode.removeChild(document.getElementById("raw"));
+})
 
 // A function to fetch things from a server
 function request(URL) {
@@ -36,25 +47,33 @@ function request(URL) {
 	return resultPromise; // Return this promise
 }
 
+function resetElements() {
+	document.getElementById("main-instruction").style.display = "block";
+	document.getElementById("stationIdentifier").style.display = "inline-block";
+	document.getElementById("getMetarButton").style.display = "inline-block";
+	document.getElementById("useLocationButton").style.display = "block";
+}
+
 // Function to get METAR provided station identifer
 function fetchMetar(station) {
 
 	var URL = "http://avwx.rest/api/metar/" + station; // This is the URL
 
 	var raw = document.createElement('div'); // This creates a new div to display the raw METAR
+	raw.id = "raw";
 	request(URL).then(function(result) { // Wait for promise to be fulfilled, and then do things with the response
 		metar = JSON.parse(result); // Parse JSON
 
 		if (metar["Raw-Report"] !== undefined) { // If there is a raw-report field in the JSON, then show that in the text
-			raw.innerHTML = metar["Raw-Report"];
+			raw.innerHTML = metar["Raw-Report"] + "<br><br>";
 		} else { // If there isn't, tell the user that their query was invalid
-			raw.innerHTML = "Your request was invalid!"
+			raw.innerHTML = "Your request was invalid!";
 		}
 
 		document.getElementById("location").appendChild(raw); // Add to the webpage!
 	}).catch(function(reason) { // This means that the query was rejected for some reason
 		console.log(reason); // Log the reason and tell the user
-		raw.innerHTML = "Your request was invalid!"
+		raw.innerHTML = "Your request was invalid!<br><br>";
 		document.getElementById("location").appendChild(raw);
     });
 }
